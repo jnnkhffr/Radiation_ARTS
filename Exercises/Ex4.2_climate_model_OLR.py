9535fe9623800593be022b78b99f4c3a89be9f3b
jnnkhffr <jannik.hoeffer@gmx.de>
2025-11-09 21:51:40 +0100

    well we shouldnt use np.flip

--- Exercises/Ex4.2_climate_model_OLR.py
+++ Exercises/Ex4.2_climate_model_OLR.py
@@ -10,24 +10,46 @@ matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import pyarts3 as pa
 
-# Importiere dein Atmosphärenmodell
+# --- Dein Atmosphärenmodell ---
 from Ex4_climate_model import climate_column
 
-# Katalogdaten sicherstellen
+# --- Hilfsfunktionen zum Schreiben von ARTS-XML ---
+def write_vector_xml(filename, values):
+    values = np.array(values, dtype=float)
+    with open(filename, "w") as f:
+        f.write('<?xml version="1.0"?>\n<ARTS>\n')
+        f.write('  <Vector>\n')
+        f.write(f'    <n>{len(values)}</n>\n')
+        f.write('    <v>' + " ".join(map(str, values)) + '</v>\n')
+        f.write('  </Vector>\n</ARTS>\n')
+
+def write_arrayofvector_xml(filename, list_of_vectors):
+    with open(filename, "w") as f:
+        f.write('<?xml version="1.0"?>\n<ARTS>\n')
+        f.write('  <ArrayOfVector>\n')
+        for vec in list_of_vectors:
+            values = np.array(vec, dtype=float)
+            f.write('    <Vector>\n')
+            f.write(f'      <n>{len(values)}</n>\n')
+            f.write('      <v>' + " ".join(map(str, values)) + '</v>\n')
+            f.write('    </Vector>\n')
+        f.write('  </ArrayOfVector>\n</ARTS>\n')
+
+# --- Katalogdaten sicherstellen ---
 pa.data.download()
 
-# Konstanten für dein Säulenmodell
+# --- Konstanten für dein Säulenmodell ---
 Ts = 290.0   # Oberflächentemperatur [K]
 Tcp = 200.0  # Tropopausentemperatur [K]
 RH = 0.8     # relative Feuchte
 
-# Säulenprofil erzeugen
+# --- Säulenprofil erzeugen ---
 p, T, x = climate_column(Ts=Ts, Tcp=Tcp, RH=RH, N=100)
-p_grid = np.flip(p).astype(float)      # Pa, aufsteigend
-T_field = np.flip(T).astype(float)     # K
-H2O_vmr = np.flip(x).astype(float)     # VMR
+p_grid = p.astype(float)      # Pa, aufsteigend
+T_field = T.astype(float)     # K
+H2O_vmr = x.astype(float)     # VMR
 
-# Höhenfeld via Hypsometrie
+# --- Höhenfeld via Hypsometrie ---
 g = 9.80665
 Rd = 287.05
 Rv = 461.5
@@ -43,30 +65,35 @@ for i in range(len(p_grid) - 1):
     Tv_bar = 0.5 * (Tv[i] + Tv[i+1])
     z_field[i+1] = z_field[i] + (Rd / g) * Tv_bar * np.log(p_grid[i] / p_grid[i+1])
 
-# Workspace
+print("p_grid:", p_grid.shape)
+print("T_field:", T_field.shape)
+print("H2O_vmr:", H2O_vmr.shape)
+print("z_field:", z_field.shape)
+#-> shapes are correct error has to be with the content of the fields
+
+print("p_grid decreasing:", np.all(np.diff(p_grid) < 0))
+print("z_field increasing:", np.all(np.diff(z_field) > 0))
+print("z_field min/max:", z_field.min(), z_field.max())
+
+
+# --- XML-Dateien schreiben ---
+write_vector_xml("p_grid.xml", p_grid)
+write_vector_xml("t_field.xml", T_field)
+write_arrayofvector_xml("vmr_field.xml", [H2O_vmr])
+write_vector_xml("z_field.xml", z_field)
+
+# --- Workspace ---
 ws = pa.workspace.Workspace()
 
-# Frequenzgitter
-kayser_grid = np.linspace(1, 2000, 200)  # cm^-1
+# Frequenzgitter (AscendingGrid: direkt NumPy-Array zuweisen)
+kayser_grid = np.linspace(200, 1500, 100)  # cm^-1, reduzierter Bereich
 ws.frequency_grid = pa.arts.convert.kaycm2freq(kayser_grid)
 
-
-# Absorptionsspezies
-ws.absorption_speciesSet(
-    species=["H2O", "H2O-ForeignContCKDMT400", "H2O-SelfContCKDMT400"]
-)
+# Absorptionsspezies (erstmal nur H2O, stabiler)
+ws.absorption_speciesSet(species=["H2O"])
 
 # Katalogdaten laden
 ws.ReadCatalogData()
-
-# Cutoff
-cutoff = pa.arts.convert.kaycm2freq(25)
-for band in ws.absorption_bands:
-    ws.absorption_bands[band].cutoff = "ByLine"
-    ws.absorption_bands[band].cutoff_value = cutoff
-
-# Linien reduzieren
-ws.absorption_bands.keep_hitran_s(approximate_percentile=90)
 
 # Absorptionsagenda
 ws.propagation_matrix_agendaAuto()
@@ -75,18 +102,14 @@ ws.propagation_matrix_agendaAuto()
 ws.surface_fieldPlanet(option="Earth")
 ws.surface_field[pa.arts.SurfaceKey("t")] = Ts
 
-# Atmosphärenfelder setzen mit ARTS-Typen
-ws.pressure_grid     = pa.arts.Vector(p_grid)
-#Breite/Länge muss NumPy-Array bleiben
-ws.latitude_grid     = np.array([0.0])
-ws.longitude_grid    = np.array([0.0])
+# Atmosphärenfelder per XML einlesen
+ws.ReadXML("p_grid.xml")
+ws.ReadXML("t_field.xml")
+ws.ReadXML("vmr_field.xml")
+ws.ReadXML("z_field.xml")
 
-ws.temperature_field = T_field[:, None, None]
-ws.z_field           = z_field[:, None, None]
-ws.vmr_field         = [H2O_vmr[:, None, None]]
-
-# Geometrie
-pos = [z_field[-1], 0.0, 0.0]   # oberste Höhe
+# Geometrie: Start knapp über Boden
+pos = [1.0, 0.0, 0.0]
 los = [180.0, 0.0]
 ws.ray_pathGeometric(pos=pos, los=los, max_step=1000.0)
 
@@ -95,12 +118,15 @@ ws.spectral_radianceClearskyEmission()
 
 # Plot
 fig, ax = plt.subplots()
-ax.plot(kayser_grid, ws.spectral_radiance[:, 0])
+ax.plot(kayser_grid, ws.spectral_radiance[:, 0], lw=2)
 ax.set_xlabel("Frequency / Kayser (cm$^{-1}$)")
 ax.set_ylabel("Spectral radiance")
 ax.set_title("Clear sky outgoing radiance (climate_column atmosphere)")
 
 if "ARTS_HEADLESS" not in os.environ:
     plt.show()
+
+
+
 
 
