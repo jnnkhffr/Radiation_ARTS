509a9297472cbd8a6135ea7d66f6ee5846fcc6ee
jnnkhffr <jannik.hoeffer@gmx.de>
2025-11-09 21:21:56 +0100

    Errors bcs no idea why ndarray is not working an Tensor3 in this pyarts3 version is not working as well....

--- Exercises/Ex4.2_climate_model_OLR.py
+++ Exercises/Ex4.2_climate_model_OLR.py
@@ -1,6 +1,6 @@
 """
 Clear-sky outgoing longwave radiation using ARTS,
-with custom single-column atmosphere from climate_column() from another py-skript.
+with custom single-column atmosphere from climate_column().
 """
 
 import numpy as np
@@ -10,70 +10,53 @@ matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import pyarts3 as pa
 
-# Lese die Funktion aus 4.1 ein
+# Importiere dein Atmosphärenmodell
 from Ex4_climate_model import climate_column
 
-# Hilfsfunktionen zum Schreiben von ARTS-XML
-def write_vector_xml(filename, values):
-    values = np.array(values, dtype=float)
-    with open(filename, "w") as f:
-        f.write('<?xml version="1.0"?>\n<ARTS>\n')
-        f.write('  <Vector>\n')
-        f.write(f'    <n>{len(values)}</n>\n')
-        f.write('    <v>' + " ".join(map(str, values)) + '</v>\n')
-        f.write('  </Vector>\n</ARTS>\n')
+# Katalogdaten sicherstellen
+pa.data.download()
 
-def write_arrayofvector_xml(filename, list_of_vectors):
-    with open(filename, "w") as f:
-        f.write('<?xml version="1.0"?>\n<ARTS>\n')
-        f.write('  <ArrayOfVector>\n')
-        for vec in list_of_vectors:
-            values = np.array(vec, dtype=float)
-            f.write('    <Vector>\n')
-            f.write(f'      <n>{len(values)}</n>\n')
-            f.write('      <v>' + " ".join(map(str, values)) + '</v>\n')
-            f.write('    </Vector>\n')
-        f.write('  </ArrayOfVector>\n</ARTS>\n')
+# Konstanten für dein Säulenmodell
+Ts = 290.0   # Oberflächentemperatur [K]
+Tcp = 200.0  # Tropopausentemperatur [K]
+RH = 0.8     # relative Feuchte
 
-# Konstanten
-Ts = 290.0   # K
-Tcp = 200.0  # K
-RH = 0.8     # 80 %
-
-# Atmosphärenprofile erzeugen
+# Säulenprofil erzeugen
 p, T, x = climate_column(Ts=Ts, Tcp=Tcp, RH=RH, N=100)
+p_grid = np.flip(p).astype(float)      # Pa, aufsteigend
+T_field = np.flip(T).astype(float)     # K
+H2O_vmr = np.flip(x).astype(float)     # VMR
 
-# ARTS erwartet aufsteigend
-p_grid = np.flip(p).astype(float)
-T_field = np.flip(T).astype(float)
-H2O_vmr = np.flip(x).astype(float)
+# Höhenfeld via Hypsometrie
+g = 9.80665
+Rd = 287.05
+Rv = 461.5
+epsilon = Rd / Rv
 
-# XML-Dateien schreiben
-write_vector_xml("p_grid.xml", p_grid)
-write_vector_xml("t_field.xml", T_field)
-write_arrayofvector_xml("vmr_field.xml", [H2O_vmr])
+e = H2O_vmr * p_grid
+r = epsilon * e / (p_grid - e)
+q = r / (1.0 + r)
+Tv = T_field * (1.0 + (Rv / Rd - 1.0) * q)
+
+z_field = np.zeros_like(p_grid)
+for i in range(len(p_grid) - 1):
+    Tv_bar = 0.5 * (Tv[i] + Tv[i+1])
+    z_field[i+1] = z_field[i] + (Rd / g) * Tv_bar * np.log(p_grid[i] / p_grid[i+1])
 
 # Workspace
 ws = pa.workspace.Workspace()
-
-ws.ReadXML("p_grid.xml")
-print("p_grid geladen")
-ws.ReadXML("t_field.xml")
-print("t_field geladen")
-ws.ReadXML("vmr_field.xml")
-print("vmr_field geladen")
-
 
 # Frequenzgitter
 kayser_grid = np.linspace(1, 2000, 200)  # cm^-1
 ws.frequency_grid = pa.arts.convert.kaycm2freq(kayser_grid)
 
+
 # Absorptionsspezies
 ws.absorption_speciesSet(
-    species=["H2O-161", "H2O-ForeignContCKDMT400", "H2O-SelfContCKDMT400"]
+    species=["H2O", "H2O-ForeignContCKDMT400", "H2O-SelfContCKDMT400"]
 )
 
-# Katalogdaten
+# Katalogdaten laden
 ws.ReadCatalogData()
 
 # Cutoff
@@ -92,9 +75,18 @@ ws.propagation_matrix_agendaAuto()
 ws.surface_fieldPlanet(option="Earth")
 ws.surface_field[pa.arts.SurfaceKey("t")] = Ts
 
-# Geometrie
-pos = [100e3, 0, 0]
+# Atmosphärenfelder setzen mit ARTS-Typen
+ws.pressure_grid     = pa.arts.Vector(p_grid)
+#Breite/Länge muss NumPy-Array bleiben
+ws.latitude_grid     = np.array([0.0])
+ws.longitude_grid    = np.array([0.0])
 
+ws.temperature_field = T_field[:, None, None]
+ws.z_field           = z_field[:, None, None]
+ws.vmr_field         = [H2O_vmr[:, None, None]]
+
+# Geometrie
+pos = [z_field[-1], 0.0, 0.0]   # oberste Höhe
 los = [180.0, 0.0]
 ws.ray_pathGeometric(pos=pos, los=los, max_step=1000.0)
 
@@ -110,3 +102,5 @@ ax.set_title("Clear sky outgoing radiance (climate_column atmosphere)")
 
 if "ARTS_HEADLESS" not in os.environ:
     plt.show()
+
+
