8c24490dba03081dc32176c639d5c1d666fd2c19
jnnkhffr <jannik.hoeffer@gmx.de>
2025-11-09 20:30:06 +0100

    Runs everytime in error. Building it completly new after this commit

--- Exercises/Ex4.2_climate_model_OLR.py
+++ Exercises/Ex4.2_climate_model_OLR.py
@@ -3,12 +3,43 @@ Clear-sky outgoing longwave radiation using ARTS,
 with custom single-column atmosphere from climate_column() from another py-skript.
 """
 import numpy as np
+import os
+import matplotlib
+matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import pyarts3 as pa  # je nach deiner Installation: from pyarts.workspace import Workspace
+#from pyarts3.io import write_xml
 #from pyarts.workspace import Workspace
 
 #Lese die Funktion aus 4.1 ein
 from Ex4_climate_model import climate_column
+
+# Download ARTS catalogs if they are not already present
+pa.data.download()
+# ARTS workspace
+ws = pa.workspace.Workspace()
+
+
+def write_vector_xml(filename, values):
+    values = np.array(values, dtype=float)
+    with open(filename, "w") as f:
+        f.write('<?xml version="1.0"?>\n<ARTS>\n')
+        f.write('  <Vector>\n')
+        f.write(f'    <n>{len(values)}</n>\n')
+        f.write('    <v>' + " ".join(map(str, values)) + '</v>\n')
+        f.write('  </Vector>\n</ARTS>\n')
+
+def write_arrayofvector_xml(filename, list_of_vectors):
+    with open(filename, "w") as f:
+        f.write('<?xml version="1.0"?>\n<ARTS>\n')
+        f.write('  <ArrayOfVector>\n')
+        for vec in list_of_vectors:
+            values = np.array(vec, dtype=float)
+            f.write('    <Vector>\n')
+            f.write(f'      <n>{len(values)}</n>\n')
+            f.write('      <v>' + " ".join(map(str, values)) + '</v>\n')
+            f.write('    </Vector>\n')
+        f.write('  </ArrayOfVector>\n</ARTS>\n')
 
 # Constants stated in the exercise
 Ts = 290.0   # K
@@ -16,17 +47,26 @@ Tcp = 200.0  # K
 RH = 0.8     # 80 %
 
 # create atmospheric profiles using the other py-file.
-p, T, x = climate_column(Ts, Tcp, RH, N=100)  # p [Pa], T [K], x [VMR]
+p, T, x = climate_column(Ts=290.0, Tcp=200.0, RH=0.8, N=100)
 
-# ARTS erwartet Druckgitter aufsteigend (klein -> groß)
-p_grid = list(np.flip(p))
-T_field = list(np.flip(T))       # K
-H2O_vmr = list(np.flip(x))       # mol/mol
+# ARTS erwartet aufsteigend
+p_grid = np.flip(p).astype(float)
+T_field = np.flip(T).astype(float)
+H2O_vmr = np.flip(x).astype(float)
 
-# Download ARTS catalogs if they are not already present
-pa.data.download()
-# ARTS workspace
-ws = pa.workspace.Workspace()
+write_vector_xml("p_grid.xml", p_grid)
+write_vector_xml("t_field.xml", T_field)
+write_arrayofvector_xml("vmr_field.xml", [H2O_vmr])
+
+
+# Atmosphärenfelder setzen
+ws.atmosphereSet1D(
+    p_grid=p_grid,
+    t_field=T_field,
+    vmr_field=[H2O_vmr],   # Liste von Spezies
+    species=["H2O"]        # Speziesnamen
+)
+
 
 # Set up frequency grid
 kayser_grid = np.linspace(1, 2000, 200)  # in Kayser (cm^-1)
@@ -67,9 +107,27 @@ ws.propagation_matrix_agendaAuto()
 #)
 
 # atmospheric grids
-ws.p_grid = p_grid
-ws.t_field = T_field
-ws.vmr_field = [H2O_vmr]
+#ws.p_grid = pa.ArrayOfFloat(p_grid.tolist())
+#ws.t_field = pa.ArrayOfFloat(T_field.tolist())
+#ws.vmr_field = pa.ArrayOfFloat([H2O_vmr.tolist()])
+# ARTS erwartet ein Vector, also konvertieren:
+#ws.p_grid = pa.arts.convert.to_vector(p_grid)
+#ws.t_field = pa.arts.convert.to_vector(T_field)
+#ws.vmr_field = [pa.arts.convert.to_vector(H2O_vmr)]
+#ws.p_grid = p_grid              # Liste von floats
+#ws.t_field = T_field            # Liste von floats
+#ws.vmr_field = [H2O_vmr]        # Liste von Listen
+#ws.p_gridSet(p_grid)
+#ws.t_fieldSet(T_field)
+#ws.vmr_fieldSet([H2O_vmr])
+
+ws.ReadXML("p_grid.xml")
+ws.ReadXML("t_field.xml")
+ws.ReadXML("vmr_field.xml")
+
+
+
+
 
 # surface
 ws.surface_fieldPlanet(option="Earth")
@@ -89,5 +147,5 @@ ax.set_xlabel("Frequency / Kayser (cm$^{-1}$)")
 ax.set_ylabel("Spectral radiance /")
 ax.set_title("Clear sky outgoing radiance (simple model)")
 
-#if "ARTS_HEADLESS" not in os.environ:
-#    plt.show()
+if "ARTS_HEADLESS" not in os.environ:
+    plt.show()
